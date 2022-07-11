7c583d6550f483d207a3911ce5f28cc11461e5e7
zumbuul <melje1delje@gmail.com>
2022-07-11 09:50:07 +0200

    quality of life changes

--- components/sign-up/SelectLanguage.tsx
+++ components/sign-up/SelectLanguage.tsx
@@ -2,14 +2,11 @@ import { NextPage } from "next";
 
 const SelectLanguage: NextPage = () => {
   return (
-    <div className="w-24 h-6 phone:h-10 phone:w-40 flex items-center shadow-sm rounded ">
-      <p className="phone:ml-5 phone:mr-5 text-grey phone:text-sm opacity-60 text-xs text-[10px] ml-2.5 mr-2">
-        English (UK)
-      </p>
-      <img
-        src="arrow.png"
-        className="phone:w-4 phone:h-2.5 w-[12] h-[6.75px] "
-      />
+    <div className="w-34 h-8 flex items-center shadow-sm rounded ">
+      <select className=" text-grey phone:text-sm opacity-60 text-xs text-[10px] ml-2.5 mr-2 px-2 py-1.5">
+        <option>English (UK)</option>
+        <option>Serbian (SR)</option>
+      </select>
     </div>
   );
 };
--- components/sign-up/SignUpHeader.tsx
+++ components/sign-up/SignUpHeader.tsx
@@ -1,14 +1,19 @@
 import { NextPage } from "next";
 import SelectLanguage from "./SelectLanguage";
+import Image from "next/image";
 
 const SingUpHeader: NextPage = () => {
   return (
     <div className="h-20 flex items-center w-screen justify-between px-8">
-      <img
-        src="Logo.png"
-        className="phone:h-[28px] phone:w-[112px] w-[73px] h-[18px]"
-        alt=""
-      />
+      {/* koristi next/image kad ubacujes slike u next, vidis da te i vsc smara da ne smes da koristis <img> */}
+
+      <Image
+        src="/Logo.png"
+        alt="Meetmax logo"
+        width="112px"
+        height="28px"
+      ></Image>
+
       <SelectLanguage />
     </div>
   );
