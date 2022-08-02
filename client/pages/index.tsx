29a07983a7fd2412490d2b8b5ee461e6bf4becf4
zumbuul <melje1delje@gmail.com>
2022-08-02 12:38:20 +0200

    added stuff

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,15 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Test: NextPage = () => {
+  const router = useRouter();
+
+  // ovo simulira kada nisi prijavljen
+  useEffect(() => {
+    router.push("/sign-up");
+  }, []);
+
   return <div className=""></div>;
 };
 
