5787f6bafe01be1b9fd352df15a619cf15b7fe4e
zumbuul <melje1delje@gmail.com>
2022-08-30 00:30:57 +0200

    added contact list

--- client/components/main/Feed.tsx
+++ client/components/main/Feed.tsx
@@ -4,7 +4,7 @@ import FeedSideCard from "./feed/FeedSideCard";
 
 const Feed: NextPage = () => {
   return (
-    <div className="w-9/12 h-[500vh] bg-grey-300 rounded-lg flex">
+    <div className="w-full h-[500vh] bg-grey-300 rounded-lg flex">
       <div className="w-full h-[100vh] m-5 rounded-lg flex flex-col gap-y-[8vh]">
         <FeedPost></FeedPost>
         <FeedPost></FeedPost>
